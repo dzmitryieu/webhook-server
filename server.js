2ba8f8a43f69ac9e675968314726e72ae851f7d7
Viktar Dzmitryieu <vitaminnus@gmail.com>
2019-08-01 13:45:50 +0300

    add endpoints

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const crypto = require('crypto');
 
-const array = [];
+let array = [];
+let arrayForRretry = [];
+let trycount = 0;
 const secret = 'secret';
 
 const app = express();
@@ -29,6 +31,7 @@ app.post('/post', function (req, res) {
     headers: req.headers,
     body: req.body,
     match,
+    date: new Date(),
   });
   if (!match) {
     res.sendStatus(403);
@@ -37,9 +40,49 @@ app.post('/post', function (req, res) {
   }
 });
 
+app.post('/post/turn', function (req, res) {
+  const signature = crypto.createHmac('sha1', secret).update(req.body).digest('hex');
+  let match = false;
+  let hash;
+  const sign = req.headers['x-elateral-signature'];
+  if (sign && sign.match(/sha1=([a-zA-Z0-9]+)/) && sign.match(/sha1=([a-zA-Z0-9]+)/)[1]) {
+    hash = sign.match(/sha1=([a-zA-Z0-9]+)/)[1];
+  } else if (!sign) {
+    hash = null;
+  }
+  if (hash === signature || hash === null) {
+    match = true;
+  }
+  arrayForRretry.push({
+    headers: req.headers,
+    body: req.body,
+    match,
+    date: new Date(),
+  });
+  if (!match && (++trycount < 3)) {
+    res.sendStatus(403);
+  } else {
+    res.sendStatus(200);
+  }
+});
+
+app.get('/cleararray', function (req, res) {
+  array = []
+  res.json(array.slice());
+});
+
+app.get('/cleararrayturn', function (req, res) {
+  arrayForRretry = []
+  res.json(arrayForRretry.slice());
+});
+
 app.get('/', function (req, res) {
   res.json(array.slice());
 });
+
+app.get('/', function (req, res) {
+  res.json(arrayForRretry.slice());
+})
 
 app.listen(process.env.PORT || 7000, function () {
   console.log('Example app listening on port 7000!');
